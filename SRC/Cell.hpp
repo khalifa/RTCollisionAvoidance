42738e5892387d365f416592aca09936a1b1c210
khalifa <samirpalumbo@gmail.com>
2013-09-16 12:46:34 -0300

    some classes

---
+++ SRC/Cell.hpp
@@ -0,0 +1,37 @@
+/******* ******* *******
+ *
+ * UNIVERSIDADE FEDERAL DE MINAS GERAIS
+ * DEPARTAMENTO DE CIÊNCIA DA COMPUTAÇÃO
+ * SEGUNDO SEMESTRE DE 2013
+ *
+ * SISTEMAS DE TEMPO REAL:
+ *      PREVENÇÃO DE COLISÃO EM CRUZAMENTOS
+ *
+ * ALUNOS:
+ *      KATTIANA CONSTANTINO  <kattiana@gmail.com>
+ *      SAMIR PALUMBO KHALIFA <khalifa@dcc.ufmg.br>
+ *      Vitor Duarte Venâncio <vdvenancio@gmail.com>
+ *
+ *
+ *
+ */
+
+#ifndef CELL_INCLUDED
+#define CELL_INCLUDED
+
+
+
+
+class Cell
+{
+	public:
+//    cruzamento
+//    ponto antes de cruzamento
+//    rua
+//    radar
+//    calcada
+
+
+};
+
+#endif
---
+++ SRC/Map.hpp
@@ -0,0 +1,31 @@
+/******* ******* *******
+ *
+ * UNIVERSIDADE FEDERAL DE MINAS GERAIS
+ * DEPARTAMENTO DE CIÊNCIA DA COMPUTAÇÃO
+ * SEGUNDO SEMESTRE DE 2013
+ *
+ * SISTEMAS DE TEMPO REAL:
+ *      PREVENÇÃO DE COLISÃO EM CRUZAMENTOS
+ *
+ * ALUNOS:
+ *      KATTIANA CONSTANTINO  <kattiana@gmail.com>
+ *      SAMIR PALUMBO KHALIFA <khalifa@dcc.ufmg.br>
+ *      Vitor Duarte Venâncio <vdvenancio@gmail.com>
+ *
+ *
+ *
+ */
+
+#ifndef MAP_INCLUDED
+#define MAP_INCLUDED
+
+#include "Cell.hpp"
+
+class Map
+{
+	public:
+    Cell **cells;
+
+};
+
+#endif
